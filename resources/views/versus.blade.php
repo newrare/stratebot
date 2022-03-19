fb1af575d55c938f04f4ad9ed8653569bcf5cd80
JulienBianchin <jurienu@gmail.com>
2022-03-19 16:21:09 +0100

    Update(Laravel): upgrade to 9 version

--- resources/views/versus.blade.php
+++ resources/views/versus.blade.php
@@ -2,5 +2,17 @@
 @section('title', 'Versus')
 
 @section('content')
-    
+    <div class="mt-10 grid grid-cols-3 md:grid-cols-6 lg:grid-cols-9 gap-5">
+        <div class="bg-emerald-300 p-5 rounded-lg">01</div>
+        <div class="bg-emerald-300 p-5 rounded-lg">02</div>
+        <div class="bg-emerald-300 p-5 rounded-lg">03</div>
+        <div class="bg-emerald-300 p-5 rounded-lg">04</div>
+        <div class="bg-emerald-300 p-5 rounded-lg">03</div>
+        <div class="bg-emerald-300 p-5 rounded-lg">03</div>
+        <div class="bg-emerald-300 p-5 rounded-lg">03</div>
+        <div class="bg-emerald-300 p-5 rounded-lg">03</div>
+        <div class="bg-emerald-300 p-5 rounded-lg">03</div>
+        <div class="bg-emerald-300 p-5 rounded-lg">09</div>
+        <div class="bg-emerald-300 p-5 rounded-lg">TOP</div>
+    </div>
 @endsection
